4734ed3ac235f1d4a374501ee7243d9e5bc7b0cb
eng-omar-gif <eng.omar.rady@gmail.com>
2025-10-12 17:37:13 +0300

    Update PlayerAudio.cpp

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -6,3 +6,48 @@
  * -----------------------------
  * Implements all functions declared in PlayerAudio.h
  */
+
+PlayerAudio::PlayerAudio() {
+    formatManager.registerBasicFormats();
+
+}
+
+
+PlayerAudio::~PlayerAudio()
+{
+    releaseResources();
+}
+
+void PlayerAudio::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void PlayerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void PlayerAudio::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+bool PlayerAudio::loadFile(const juce::File& file)
+{
+    if (file.existsAsFile())
+    {
+        if (auto* reader = formatManager.createReaderFor(file))
+        {
+            transportSource.stop();
+            transportSource.setSource(nullptr);
+            readerSource.reset();
+
+            readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+            transportSource.setSource(readerSource.get(), 0, nullptr, reader->sampleRate);
+            transportSource.start();
+            return true;
+        }
+    }
+    return false;
+}
