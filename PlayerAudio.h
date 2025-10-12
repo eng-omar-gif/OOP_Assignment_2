5bee77edb767397056330205810c8afcb586a8e5
eng-omar-gif <eng.omar.rady@gmail.com>
2025-10-12 17:37:53 +0300

    Update PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,8 +1,41 @@
-
 #pragma once
 #include <JuceHeader.h>
+#include "MainComponent.h"
+
 /*
  * PlayerAudio.h
  * -----------------------------
  * Handles the AUDIO logic (backend) of the player.
  */
+ 
+class PlayerAudio {
+private:
+
+
+    // Audio
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
+public:
+    PlayerAudio();
+    ~PlayerAudio();
+    //Audio
+    
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
+ 
+    bool loadFile(const juce::File& file);
+    void play();
+    void stop();
+    void setGain(float gain);
+    void setPosition(double pos);
+    double getPosition() const;
+    double getLength() const;
+
+
+    
+
+
+};
